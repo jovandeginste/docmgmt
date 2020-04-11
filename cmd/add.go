b038f6c129d75081dce3420cc2d24cc52e27be66
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-04-11 17:53:35 +0200

    Switch to "add" command
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

---
+++ cmd/add.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:  "add file... [--tag t1...]",
+	Long: `Parse files and add it with some tags to the document management`,
+	Args: cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		add(args, tags)
+	},
+}
+var tags []string
+
+func init() {
+	RootCmd.AddCommand(addCmd)
+	addCmd.Flags().StringSliceVarP(&tags, "tag", "t", []string{}, "Tag to add")
+}
+
+func add(files []string, tags []string) {
+	err := myApp.StartServer()
+	if err != nil {
+		panic(err)
+	}
+	defer myApp.StopServer()
+
+	for _, file := range files {
+		i, err := myApp.Parse(file)
+		if err != nil {
+			panic(err)
+		}
+		i.AddTags(tags)
+		err = i.Write()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
