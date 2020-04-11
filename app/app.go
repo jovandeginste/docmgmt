4de1a7d177580ce3f4fd5a59ee32ee3641fce9ff
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-04-11 17:43:57 +0200

    Make DocumentRoot the required root
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- app/app.go
+++ app/app.go
@@ -2,13 +2,14 @@ package app
 
 import (
 	"context"
+	"errors"
 	"log"
+	"path/filepath"
+	"strings"
 
 	"github.com/google/go-tika/tika"
 	"github.com/jinzhu/gorm"
 	"github.com/jovandeginste/docmgmt/bayes"
-	"github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
 
 type App struct {
@@ -16,54 +17,6 @@ type App struct {
 	DB            *gorm.DB
 	Classifier    *bayes.Classifier
 	TikaServer    *tika.Server
-}
-
-func (a *App) LoadConfiguration(cfgFile string) error {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Search config in home directory with name ".cobra-example" (without extension).
-		viper.AddConfigPath("$HOME/.docmgmt")
-		viper.SetConfigName("config")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-			panic(err)
-		}
-	}
-	err := viper.Unmarshal(&a.Configuration)
-	if err != nil {
-		return err
-	}
-
-	err = a.InitDB()
-	if err != nil {
-		return err
-	}
-
-	err = a.LoadClassifier()
-	if err != nil {
-		return err
-	}
-
-	p, err := homedir.Expand("~/.docmgmt/tika-server-" + a.Configuration.TikaVersion + ".jar")
-	if err != nil {
-		return err
-	}
-
-	s, err := tika.NewServer(p, "")
-	if err != nil {
-		return err
-	}
-
-	a.TikaServer = s
-
-	return nil
 }
 
 func (a *App) StartServer() error {
@@ -81,7 +34,19 @@ func (a *App) StopServer() {
 }
 
 func (a *App) ReadFileInfo(file string) (result *Info, err error) {
-	result = &Info{Filename: file}
+	path, err := filepath.Abs(file)
+	if err != nil {
+		return
+	}
+
+	if !strings.HasPrefix(path, a.Configuration.DocumentRoot) {
+		err = errors.New("file is not inside document root")
+		return
+	}
+
+	path = strings.TrimPrefix(path, a.Configuration.DocumentRoot)
+
+	result = &Info{Filename: path}
 	result.App = a
 
 	err = a.DB.Where(result).FirstOrInit(&result).Error
--- app/config.go
+++ app/config.go
@@ -1,9 +1,69 @@
 package app
 
+import (
+	"strings"
+
+	"github.com/google/go-tika/tika"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
 type Configuration struct {
 	Verbose        bool
 	DocumentRoot   string `mapstructure:"document_root"`
 	MetadataDB     string `mapstructure:"metadata_db"`
 	TikaVersion    string `mapstructure:"tika_version"`
 	ClassifierData string `mapstructure:"classifier_data"`
+}
+
+func (a *App) LoadConfiguration(cfgFile string) error {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Search config in home directory with name ".cobra-example" (without extension).
+		viper.AddConfigPath("$HOME/.docmgmt")
+		viper.SetConfigName("config")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			panic(err)
+		}
+	}
+	err := viper.Unmarshal(&a.Configuration)
+	if err != nil {
+		return err
+	}
+
+	err = a.InitDB()
+	if err != nil {
+		return err
+	}
+
+	err = a.LoadClassifier()
+	if err != nil {
+		return err
+	}
+
+	p, err := homedir.Expand("~/.docmgmt/tika-server-" + a.Configuration.TikaVersion + ".jar")
+	if err != nil {
+		return err
+	}
+
+	s, err := tika.NewServer(p, "")
+	if err != nil {
+		return err
+	}
+
+	if !strings.HasSuffix(a.Configuration.DocumentRoot, "/") {
+		a.Configuration.DocumentRoot = a.Configuration.DocumentRoot + "/"
+	}
+
+	a.TikaServer = s
+
+	return nil
 }
